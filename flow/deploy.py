7192d271358f70d49b405137ac48feda01bad5bb
Farbod Abolhassani <farbod.ab@hotmail.ca>
2024-12-01 11:18:09 -0500

    Update prefect

---
+++ flow/deploy.py
@@ -0,0 +1,16 @@
+from main import monitoring_flow
+from pathlib import Path
+
+
+if __name__ == "__main__":
+
+    source = str(Path(__file__).parent.parent)
+
+    monitoring_flow.from_source(
+        source=source,
+        entrypoint="/flow/main.py:monitoring_flow",
+    ).deploy(
+        name="monitoring-flow",
+        work_pool_name="monitoring-pool",
+        push=False,
+    )
--- flow/start.sh
+++ flow/start.sh
@@ -3,15 +3,13 @@
 # Allow prefect server to start
 sleep 5
 
+# define the workpool
+prefect work-pool create --type process monitoring-pool
+
 # Deploy monitoring flow
-prefect deployment build flow/main.py:monitoring_flow \
-                      -n monitoring-flow \
-                      -q monitoring-pool \
-                      -o prefect.yaml \
-                      --skip-upload \
-                      --apply
+python /app/flow/deploy.py --concurrency-limit 5
 
 # Start the agent
-prefect agent start -q 'monitoring-pool' --limit 1
+prefect worker start -p 'monitoring-pool'
 
 
